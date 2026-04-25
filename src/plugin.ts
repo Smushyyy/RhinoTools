9f0944d3ec4948cc404b7933ac1a10b27a4b720d
Philip Patrick Blomberg Karlsson <Philip.blomberg@outlook.com>
2026-04-25 15:39:15 +0200

    Wrap logic in async IIFE for top-level await compatibility.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,6 +1,8 @@
 import streamDeck, { LogLevel } from "@elgato/streamdeck";
 import { RhinoToolAction } from "./actions/rhino-tool";
 
-streamDeck.logger.setLevel(LogLevel.TRACE);
-streamDeck.actions.registerAction(new RhinoToolAction());
-await streamDeck.connect();
+(async () => {
+    streamDeck.logger.setLevel(LogLevel.TRACE);
+    streamDeck.actions.registerAction(new RhinoToolAction());
+    await streamDeck.connect();
+})();
